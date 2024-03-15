5fde601b14e09a29dd3055a360f9402d7abba036
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-15 16:56:41 -0300

    feat: initial files

---
+++ src/test/java/io/github/Ital023/LoansAPI/LoansApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.LoansAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
